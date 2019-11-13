f588bc3175d109ef58d38987502287c5b00c6b9c
StriderWilson <strider.w@berkeley.edu>
2019-11-12 17:16:10 -0800

    Complete Promises

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -9,12 +9,34 @@
  */
 
 var fs = require('fs');
+var gitHub = require('./promisification');
 var Promise = require('bluebird');
+var db = Promise.promisifyAll(require('../../lib/db'));
+var pc = require('./promiseConstructor');
 
-
-
-var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
+var fetchProfileAndWriteToFile = function (readFilePath, writeFilePath) {
   // TODO
+  // return new Promise(function (resolve, reject) {
+  //   console.log('first line ', pc.pluckFirstLineFromFileAsync(readFilePath));
+  // })
+  return pc.pluckFirstLineFromFileAsync(readFilePath)
+    .then((firstLine) => {
+      return gitHub.getGitHubProfileAsync(firstLine);
+    })
+    .then((body) => {
+
+      fs.writeFileSync(writeFilePath, JSON.stringify(body), (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('Success');
+        }
+      });
+    })
+    .catch(function (error) {
+      console.log('error is -----', error);
+    });
+
 };
 
 // Export these functions so we can test them
